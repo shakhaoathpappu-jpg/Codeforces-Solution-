1370c087a5dbc9c38d28685d234ca5c5ce202e35
Shakhaoath Pappu <shakhaoathpappu@gmail.com>
2026-06-30 21:03:02 +0600

    Codeforces 2241A Solution

---
+++ codeforces 2241a.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int t; cin >> t;
+    while(t--){
+        int x, y; cin >> x >> y;
+        if(x % y == 0) cout << "YES\n";
+        else cout << "NO\n";
+    }
+}
