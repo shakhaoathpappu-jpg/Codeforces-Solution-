79ed37b2d88be0f557cca1af2195ab7795c22082
Shakhaoath Pappu <shakhaoathpappu@gmail.com>
2026-07-09 16:08:43 +0600

    Codeforces 2242C Solution

--- codeforces 2242c.cpp
+++ codeforces 2242c.cpp
@@ -1,40 +1,74 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main() 
-{
+constexpr int N = 5e3 + 20;
+int dp[N][N];
+
+vector<vector<int>> makeMinLen(string s) {
+    const int n = (int) s.size();
+    vector<vector<int>> minPos(n, vector<int>(10, N));
+    for (int i = 0; i < n; i++) {
+        int sum = 0;
+        for (int j = i; j < n; j++) {
+            sum += s[j] - '0';
+            if (sum >= 10) {
+                sum -= 10;
+            }
+            minPos[i][sum] = min(minPos[i][sum], j);
+        }
+    }
+    return minPos;
+};
+
+int main() {
+#ifndef LOCAL
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#endif
+
     int t; cin >> t;
-    while(t--) {
-        int n;
-        long long k; cin >> n >> k;
-        vector<int> a(n);
-        for(auto &x : a) cin >> x;
-        vector<long long> f(n + 2, 0);
-        int i = 0;
-        while (i < n) {
-            int j = i;
-            while (j < n && a[j] == a[i]) j++;
-            int l = j - i;
-            f[l]++;
-            i = j;
+    while (t--) {
+        string a, b;
+        cin >> a >> b;
+        const int n = (int) a.size(), m = (int) b.size();
+
+        vector<vector<int>> minPosA = makeMinLen(a), minPosB = makeMinLen(b);
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                dp[i][j] = -N;
+            }
         }
-        vector<long long> c(n + 2, 0), s(n + 2, 0);
-        for (int i = n; i >= 1; i--) {
-            c[i] = c[i + 1] + f[i];
-            s[i] = s[i + 1] + 1LL * i * f[i];
-        }
-        long long ans = 0;
-        for (int i = 1; i <= n; i++) {
-            if (f[i] > 0) {
-                long long x = c[i];
-                long long y = s[i];
-                long long d = k - y;
-                if(d % x == 0) {
-                    long long z = d / x;
-                    if (z >= 1 - i) ans++;
+        dp[0][0] = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int k = 0; k < 10; k++) {
+                    int ma = minPosA[i][k], mb = minPosB[j][k];
+                    if (ma != N && mb != N) {
+                        dp[ma + 1][mb + 1] = max(dp[ma + 1][mb + 1], dp[i][j] + 1);
+                    }
                 }
             }
         }
-        cout << ans << '\n';
+
+        int answer = -1;
+        int sumA = 0;
+        for (int i = n; i > 0; i--) {
+            int sumB = 0;
+            for (int j = m; j > 0; j--) {
+                if (sumA == 0 && sumB == 0) {
+                    answer = max(answer, dp[i][j]);
+                }
+                sumB += b[j - 1] - '0';
+                if (sumB >= 10) {
+                    sumB -= 10;
+                }
+            }
+
+            sumA += a[i - 1] - '0';
+            if (sumA >= 10) {
+                sumA -= 10;
+            }
+        }
+        cout << answer << endl;
     }
 }
